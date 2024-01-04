30d22885fb6c90bc30b954504661eee32c68e547
SaadMinhas78 <muhammadsaad008800@gmail.com>
2024-01-04 17:15:59 +0500

    feat: admin id updated

--- services/conversation.ts
+++ services/conversation.ts
@@ -5,32 +5,28 @@ const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 const Conversation = {
   getUserConversation: async (userId: string, token?: string) => {
     const config = {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      };
-    return await axios.get(
-        `${baseUrl}/conversation/${userId}`,
-        config
-      )
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    return await axios
+      .get(`${baseUrl}/conversation/${userId}`, config)
       .then((res) => res.data)
       .catch((error) => toast.error(error?.response?.data?.message));
   },
+
   createNewConversation: async (userId: string, token?: string) => {
     const config = {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      };
-      const body = {
-        senderId: userId,
-        receiverId: "658bf667f5a854fe616789d5"
-    }
-    return await axios.post(
-        `${baseUrl}/conversation/`,
-        body,
-        config
-      )
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const body = {
+      senderId: userId,
+      receiverId: "6593b60fd2cb3f4a49180156",
+    };
+    return await axios
+      .post(`${baseUrl}/conversation/`, body, config)
       .then((res) => res.data)
       .catch((error) => toast.error(error?.response?.data?.message));
   },
